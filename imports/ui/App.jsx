d9dea5ef3cd1bf50c57d388e16b056a5df9b4552
KevvKo <kklein915@gmail.com>
2022-01-06 11:43:51 +0100

    add routing

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Login } from './views/Login';
+import  { Index } from './views/Index';
+import  { Login }  from './views/Login';
+import  { Registration }  from './views/Registration';
+import {
+  BrowserRouter as Router,
+  Route, 
+  Routes
+} from "react-router-dom";
 import { useTracker } from 'meteor/react-meteor-data';
 
 export const App = () => {
-
   const user = useTracker(() => Meteor.user())
   return(
-    <div>
-      { user ? (
-        <h1>Welcome</h1>
-      )
-      : (
-        <Login />
-      )
-      }
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Index />}></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="registration" element={<Registration />}></Route>          
+        </Routes>
+      </div>
+    </Router>
   )
 };
--- imports/ui/views/Registration.js
+++ imports/ui/views/Registration.js
@@ -4,11 +4,11 @@ import React from 'react';
 export const Registration = () => {
     return (
         <form>
-            <label for="username">Username:</label>
+            <label htmlFor="username">Username:</label>
             <input type="text" name="username"/>
-            <label for="email">Email:</label>
+            <label htmlFor="email">Email:</label>
             <input type="text" name="email"/>
-            <label for="username">Password:</label>
+            <label htmlFor="password">Password:</label>
             <input name="password" type="password"/>
         </form>
     )
