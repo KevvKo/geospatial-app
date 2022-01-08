e957942e52f9dddd6e7c8c6f339f0fe4392f5a3b
KevvKo <kklein915@gmail.com>
2022-01-08 23:43:51 +0100

    add private route

--- imports/ui/views/Index.js
+++ imports/ui/views/Index.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useTracker } from 'meteor/react-meteor-data';
 
 export const Index = () => {
+
+    const navigate = useNavigate();
+    const user = useTracker(() => Meteor.user());
+
+    useEffect(() => {
+        if(!user) navigate('../login')
+    }, []);
     return (
+
         <div>hello</div>
     )
 }
