ceb00b61814b74392ecb6cab2de0d9664f7889b8
KevvKo <kklein915@gmail.com>
2022-01-10 23:45:46 +0100

    update toolbar with logout

--- imports/ui/views/Index.js
+++ imports/ui/views/Index.js
@@ -1,17 +1,30 @@
 import React, { useEffect } from "react";
+import { Meteor } from 'meteor/meteor';
 import { useNavigate } from "react-router-dom";
 import { useTracker } from 'meteor/react-meteor-data';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
 
 export const Index = () => {
 
     const navigate = useNavigate();
     const user = useTracker(() => Meteor.user());
+    handleClick = () => {
+        Meteor.logout();
+    }
 
     useEffect(() => {
         if(!user) navigate('../login')
     });
-    return (
 
-        <div>hello</div>
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <Button onClick={ handleClick } color='inherit' sx={{ marginLeft: 'auto'}}>Sign out</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
     )
 }
